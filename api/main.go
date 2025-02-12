19626da3e7a27e7c06f303c020543a6c710626eb
nineee02 <wnueangwong@gmail.com>
2025-02-12 15:34:16 +0700

    12/2/2568

--- api/main.go
+++ api/main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"log"
-	"nirun/pkg/event"
 	"nirun/pkg/hook"
 	"nirun/pkg/linebot"
 
 	"github.com/gin-gonic/gin"
-	"github.com/skip2/go-qrcode"
 )
 
-func generateQRCode() {
-	url := "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006878417&redirect_uri=http%3A%2F%2Fcommunity.app.nirun.life%2Fauth_oauth%2Fsignin&state=random_string&scope=profile+openid+email"
-	err := qrcode.WriteFile(url, qrcode.Medium, 256, "qrcode_line_login.png")
-	if err != nil {
-		log.Fatalf("Failed to generate QR Code: %v", err)
-	}
-	// log.Println("QR Code generated successfully.")
-}
+// func generateQRCode() {
+// 	url := "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006878417&redirect_uri=http%3A%2F%2Fcommunity.app.nirun.life%2Fauth_oauth%2Fsignin&state=random_string&scope=profile+openid+email"
+// 	err := qrcode.WriteFile(url, qrcode.Medium, 256, "qrcode_line_login.png")
+// 	if err != nil {
+// 		log.Fatalf("Failed to generate QR Code: %v", err)
+// 	}
+// 	// log.Println("QR Code generated successfully.")
+// }
 
 func main() {
 	// สร้าง QR Code สำหรับ URL
-	generateQRCode()
+	// generateQRCode()
 
 	// รันเซิร์ฟเวอร์ของ LINE Bot
 	// (โค้ดที่เหลือของคุณสำหรับการตั้งค่า Gin และเซิร์ฟเวอร์)
@@ -29,8 +26,8 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/login", event.LineLoginHandler)
-	router.GET("/auth_oauth/signin", event.LineLoginCallback)
+	// router.GET("/login", event.LineLoginHandler)
+	// router.GET("/auth_oauth/signin", event.LineLoginCallback)
 	// router.GET("/callback", event.LineLoginCallback)
 	router.POST("/webhook", hook.HandleLineWebhook)
 
